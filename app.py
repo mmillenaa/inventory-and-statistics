dcf9fad2e12029a9183e2f205e51402befd01783
Millena Miranda Franco <102330737+mmillenaa@users.noreply.github.com>
2026-08-14 15:10:45 -0300

    Updating the title

--- app.py
+++ app.py
@@ -520,7 +520,7 @@ html_arvore = """
 </details>
 
 <details>
-<summary>Subsérie: Penitenciárias e presídios - Casa de Detenção do Carandiru (jornal Diários Associados do Estado de São Paulo — DASP)</summary>
+<summary>Subsérie: Penitenciárias e presídios - Casa de Detenção de São Paulo no Carandiru (jornal Diários Associados do Estado de São Paulo — DASP)</summary>
 <div class="item-simples"><span class="status-badge bg-vermelho">🔴 Pronta, mas aguardando autorização para uso em futuras bases de dados.</span></div>
 <div class="item-simples"><span class="tag-azul">BR-SPAPESP_DASP-PENITPRE-CSDTCARANDIRU_TXT-PNL-MT0_0001.xlsx</span></div>
 </details>
@@ -666,7 +666,7 @@ with aba_equipe:
 
     st.markdown("<br><hr>", unsafe_allow_html=True)
     
-    st.subheader(traduzir("Observatório de bases publicadas pelo GPDVE no Dataverse"))
+    st.subheader(traduzir("Observatório de bases publicadas pelo GPDVE no Dataverse da FGV"))
     st.markdown("Listagem automatizada das publicações institucionais das autoras do GPDVE.")
     
     # === AQUI: Utilizando as duas chaves de API ===
